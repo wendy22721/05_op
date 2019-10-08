2ad17e11cb233bdb3c390774e76e739f2803b5e5
wendy22721 <55476967+wendy22721@users.noreply.github.com>
2019-10-08 16:21:46 +0900

    Create 07.c

---
+++ 07.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int answer = 59;
+	int input;
+	int cnt=0;
+	
+	do
+	{
+		printf("input a number :");
+		scanf("%d", &input);
+		if (input < answer)
+		{
+			printf("low!\n");
+		}
+		else if (input > answer)
+		{
+			printf("high!\n");
+		}
+		cnt++;
+	}
+	
+	while( input != answer );
+	printf("congratulation. trial : %i\n", cnt);
+	
+	return 0;
+}
