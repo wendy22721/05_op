4cd92ad94e55a5ade211d96c5be8e060040933e0
wendy22721 <55476967+wendy22721@users.noreply.github.com>
2019-10-08 15:14:10 +0900

    Create 04.c

---
+++ 04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i=0, meter;
+	
+	while (i<3)
+{	
+	meter = i * 1609;
+	printf("%d 마일은%d 미터입니다.\n", i, meter);
+	i++;
+}
+	return 0;
+}
