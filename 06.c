d3b6af2a2a38e58f8a8adf25a98e7ce20a74602c
wendy22721 <55476967+wendy22721@users.noreply.github.com>
2019-10-08 16:02:39 +0900

    Create 06.c

---
+++ 06.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int num = 0;
+	char c;
+	
+	printf("input a string : ");
+	while ((c = getchar()) != '\n')
+	{
+		if (c<='9'&& c>='0')
+		{
+			num++;
+		}
+	}
+	
+	printf("%i\n", num);
+	
+	return 0;
+}
